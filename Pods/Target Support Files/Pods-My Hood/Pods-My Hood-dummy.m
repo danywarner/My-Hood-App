7b09372712b01c6174a71e8b838cf9bc68b4e3d7
Daniel Warner <danywarner@gmail.com>
2015-12-10 18:39:15 -0500

    keyboard not hidding

---
+++ Pods/Target Support Files/Pods-My Hood/Pods-My Hood-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_My_Hood : NSObject
+@end
+@implementation PodsDummy_Pods_My_Hood
+@end
---
+++ Pods/Target Support Files/Pods-My Hood/Pods-My Hood-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_My_HoodVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_My_HoodVersionString[];
+
